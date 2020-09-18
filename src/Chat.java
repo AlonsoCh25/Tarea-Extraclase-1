15c507790da7d6f528e0faa4e8a9c4be3a0cc31a
Kenneth C <kennethalonsoc@gmail.com>
2020-09-18 12:31:30 -0600

    Update; Delete Items and create new methods for Client and server

--- src/Chat.java
+++ src/Chat.java
@@ -1,54 +1,14 @@
 //import the libraries
+import com.sun.xml.internal.bind.v2.model.annotation.RuntimeAnnotationReader;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
 //Principal class of the APP
-public class Chat extends javax.swing.JFrame implements Observer , Runnable{
-    int port;
-    public Chat(){
-        this.getRootPane().setDefaultButton(this.btnSend);
-        btnSend.addActionListener(e -> {
-            String message = "1: " + txtMessage.getText() + "\n";
-            txtChat.append(message);
-            Client c = new Client(message, port, Play.chat);
-            Thread t1 = new Thread(c);
-            t1.start();
-        });
+public class Chat{
+    public Chat() {
     }
-    //Creation of private variables
-    private JTextArea txtChat;
-    private JButton btnSend;
-    private JTextField txtMessage;
-    private JPanel Panel;
-
-    @Override
-    //method that makes the observer update
-    public void update(Observable o, Object arg){
-        System.out.println("Observador actualizado");
-        this.txtChat.append((String) arg);
-        System.out.println(this.txtChat.getText());
-    }
-
-    @Override
-    public void run() {
-        Server s = new Server(6000);
-        s.addObserver(this);
-        Thread t = new Thread(s);
-        t.start();
-        }
-    static class Play {
-        private static int port_ = 0;
-        private static int chat = 1;
-        public static void main(String[] args){
-            for(int i=1;i <= chat; i+=1){
-                System.out.println("Veces ejecutadas");
-                port_ += 1;
-                Chat c = new Chat();
-                c.port = port_+5000;
-                Thread t = new Thread(c);
-                t.start();
-            }
-        }
-    }
-
 }
--- src/Server.java
+++ src/Server.java
@@ -1,52 +1,18 @@
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Observable;
 
-//Create the Server class, with its characteristics
-public class Server extends Observable implements Runnable{
-    //Create the private varibles
+public class Server{
+    private ServerSocket socket;
     private int port;
-    //Function that assigns the port value
+
     public Server(int port){
         this.port = port;
-        System.out.println("Server Port " + port);
     }
 
-    @Override
-    //Function that creates and connects to the server
-    public void run() {
-        //Create the varibles
-        ServerSocket server;
-        Socket SC;
-        DataInputStream IN;
-        //Try to create the server
-        try{
-            //Create the server
-            server = new ServerSocket(port);
-            System.out.println("Started Server");
-            while (true){
-                //Wait for a client to connect
-                SC = server.accept();
-                System.out.println("Client Connected");
-                //wait for information
-                IN = new DataInputStream(SC.getInputStream());
-                //Assign the information received to a variable
-                String message = IN.readUTF();
-                //Whait for any changes and notify the Observers
-                this.setChanged();
-                this.notifyObservers(message);
-                this.clearChanged();
-                //Close the server
-                SC.close();
-                System.out.println("Disconnected Client");
-            }
-        }
-        //Catch method
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public void init() throws IOException {
+        socket = new ServerSocket(port)
+    }
+    public void close() throws IOException{
+        socket.close();
     }
 }
